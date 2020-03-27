52faf2981ba3176989131030c23f3d3147f58bcf
蕭立品 <a0979225002@gmail.com>
2020-03-27 23:06:58 +0800

    底層傳輸方法:抓取json資料傳輸在兩個listview中

--- app/src/main/java/com/example/lipin/json/contentActivity.java
+++ app/src/main/java/com/example/lipin/json/contentActivity.java
@@ -12,8 +12,26 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -47,18 +65,84 @@ public class contentActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    URL url = new URL(strPic);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
+//                    URL url = new URL(strPic);
+//                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                    connection.connect();
                     //將圖片轉成BitMap
-                    bitmap = BitmapFactory.decodeStream(connection.getInputStream());
-                    uiHandler.sendEmptyMessage(0);
+
+                    // Load CAs from an InputStream
+                    // (could be from a resource or ByteArrayInputStream or ...)
+                    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+                    // From https://www.washington.edu/itconnect/security/ca/load-der.crt
+                    InputStream caInput = new BufferedInputStream(new FileInputStream("load-der.crt"));
+                    Certificate ca;
+                    try {
+                        ca = cf.generateCertificate(caInput);
+                        System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+                    } finally {
+                        caInput.close();
+                    }
+
+                    // Create a KeyStore containing our trusted CAs
+                    String keyStoreType = KeyStore.getDefaultType();
+                    KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+                    keyStore.load(null, null);
+                    keyStore.setCertificateEntry("ca", ca);
+
+                    // Create a TrustManager that trusts the CAs in our KeyStore
+                    String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+                    TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+                    tmf.init(keyStore);
+
+                    // Create an SSLContext that uses our TrustManager
+                    SSLContext context = SSLContext.getInstance("TLS");
+                    context.init(null, tmf.getTrustManagers(), null);
+
+                    // Tell the URLConnection to use a SocketFactory from our SSLContext
+                    URL url = new URL(strPic);
+                    HttpsURLConnection urlConnection =
+                            (HttpsURLConnection)url.openConnection();
+                    urlConnection.setSSLSocketFactory(context.getSocketFactory());
+                    InputStream in = urlConnection.getInputStream();
+                    copyInputStreamToOutputStream(in);
+
+
+
+
 
                 } catch (Exception e) {
-
+                    Log.v("brad",e.toString());
                 }
             }
+
+            private void copyInputStreamToOutputStream(InputStream in) {
+                bitmap = BitmapFactory.decodeStream(in);
+                uiHandler.sendEmptyMessage(0);
+
+            }
         }.start();
+    }
+    private void fetchImage2(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        ImageRequest request = new ImageRequest(
+                strPic,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        img.setImageBitmap(response);
+                    }
+                },
+                0, 0,
+                Bitmap.Config.ARGB_8888,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("brad", error.toString());
+                    }
+                }
+        );
+        request.setShouldRetryServerErrors(true);
+        queue.add(request);
     }
     private class UIHandler extends Handler{
         @Override
